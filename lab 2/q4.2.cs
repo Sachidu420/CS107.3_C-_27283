485041c7e585c2ccec412d0d2a7bb547607f0b15
sachindu <135845933+Sachidu420@users.noreply.github.com>
2023-07-31 17:54:57 +0530

    Add files via upload

---
+++ lab 2/q4.2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace HelloWorldNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld helloObj = new HelloWorld();
+            helloObj.SayHello(); 
+            Console.ReadLine(); 
+        }
+    }
+}
